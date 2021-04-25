05184cf1d0b89cf64ec34a2feaf35d222da2316c
Tutku Ucan <72797914+tutklon@users.noreply.github.com>
2021-04-25 13:49:36 +0300

    Update ui.js

--- public/js/ui.js
+++ public/js/ui.js
@@ -8,3 +8,13 @@ for (i = 0; i < close.length; i++) {
     setTimeout(function(){ div.style.display = "none"; }, 600);
   }
 }
+
+const page_title = document.getElementById('page-title')
+const title_input = document.getElementById('title-input')
+
+title_input.addEventListener('keyup' , (e) => {
+ if (page_title.innerText.length <= 0) {
+   return page_title.innerText = "add note" 
+ }
+ else page_title.innerText = e.target.value
+})
